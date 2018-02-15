b3f31946cf9b9e6823ee3ace5b8a950886a9b78e
i367759 <nick_gorajski@bcbsil.com>
2018-02-15 06:59:16 -0600

    Initial commit

---
+++ src/main/java/com/gorajski/javaanonymousblognodb/JavaAnonymousBlogNodbApplication.java
@@ -0,0 +1,12 @@
+package com.gorajski.javaanonymousblognodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAnonymousBlogNodbApplication {
+
+	public static void main(String[] args) {
+	    SpringApplication.run(JavaAnonymousBlogNodbApplication.class, args);
+	}
+}
