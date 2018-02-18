75a03accc0d401dacc86f47133fc343cca8e5cc6
i367759 <nick_gorajski@bcbsil.com>
2018-02-18 15:10:30 -0600

    Add routes to fetch new Post form and to handle form submission

--- src/main/java/com/gorajski/javaanonymousblognodb/controllers/PostController.java
+++ src/main/java/com/gorajski/javaanonymousblognodb/controllers/PostController.java
@@ -4,9 +4,7 @@ import com.gorajski.javaanonymousblognodb.models.Post;
 import com.gorajski.javaanonymousblognodb.repositories.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,14 +18,21 @@ public class PostController {
         return "index";
     }
 
+    @RequestMapping(value="/posts", method=RequestMethod.POST)
+    public String postCreate(@RequestParam("title") String title, @RequestParam("author") String author, @RequestParam("body") String body) {
+        PostRepository.createPost(title, author, body);
+        String str = body;
+        return "redirect:/";
+    }
+
     @RequestMapping("/posts/{id}")
     public String postShow(@PathVariable int id, ModelMap map) {
         map.put("post", PostRepository.findPostById(id));
         return "show";
     }
 
-//    @RequestMapping("/posts/new")
-//    public String postNew(ModelMap map) {
-//        Post post
-//    }
+    @RequestMapping("/posts/new")
+    public String postNew() {
+        return "new";
+    }
 }
--- src/main/java/com/gorajski/javaanonymousblognodb/repositories/PostRepository.java
+++ src/main/java/com/gorajski/javaanonymousblognodb/repositories/PostRepository.java
@@ -2,15 +2,16 @@ package com.gorajski.javaanonymousblognodb.repositories;
 
 import com.gorajski.javaanonymousblognodb.models.Post;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class PostRepository {
-    private static List<Post> posts = Arrays.asList(
+    private static List<Post> posts = new ArrayList<Post>(Arrays.asList(
             new Post("Chicken Soup", "Glenn Campbell", "It's good for the soul."),
             new Post("Heavy Metal", "Terk Siddiqui", "I love heavy metal.  Makes me feel free to be myself."),
             new Post("Crocheting", "Barry Karlov", "Today we're going to learn a new technique.")
-    );
+    ));
 
     public static List<Post> allPosts() {
         return posts;
@@ -24,5 +25,11 @@ public class PostRepository {
             }
         }
         return null;
+    }
+
+    public static Post createPost(String title, String author, String body) {
+        Post post = new Post(title, author, body);
+        posts.add((Post) post);
+        return post;
     }
 }
